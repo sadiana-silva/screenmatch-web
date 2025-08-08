5dce9b00cca44f64f6ac3d8554f4e3dd2ec4d0f7
sadiana <dev.sadiana@gmail.com>
2025-08-08 18:26:06 -0300

    representing database series with SerieDTO

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class ScreenmatchApplication {
 
 	public static void main(String[] args) {
