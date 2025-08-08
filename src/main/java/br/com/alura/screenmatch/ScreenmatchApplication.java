da2d29cdf5ef73074d00970279e59afd896a5678
sadiana <dev.sadiana@gmail.com>
2025-08-08 12:52:20 -0300

    restructuring the spring application class

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -6,19 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class ScreenmatchApplication implements CommandLineRunner {
-	@Autowired
-	private SerieRepository repositorio;
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class ScreenmatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ScreenmatchApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		Principal principal = new Principal(repositorio);
-		principal.exibeMenu();
 	}
 }
