d614955dd1bea922aea89890a52fa938cb09a8fa
sadiana <dev.sadiana@gmail.com>
2025-08-14 19:32:41 -0300

    returning series details

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -5,6 +5,7 @@ import br.com.alura.screenmatch.model.Serie;
 import br.com.alura.screenmatch.service.SerieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +31,10 @@ public class SerieController {
     @GetMapping("/lancamentos")
     public List<SerieDTO> obterLancamento() {
         return servico.obterLancamento();
+    }
+
+    @GetMapping ("/{id}")
+    public SerieDTO obterPorId(@PathVariable Long id) {
+        return servico.obterPorId(id);
     }
 }
